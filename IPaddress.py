c99be65704557cf5018eb817a17faa26419d8508
herbie-c <74236333+herbie-c@users.noreply.github.com>
2021-01-17 18:57:27 +0100

    Update IPaddress.py

--- IPaddress.py
+++ IPaddress.py
@@ -1,5 +1,6 @@
 import subprocess
 t_output = str(subprocess.run('/home/pi/IPaddress.sh',capture_output=True,shell=True)
 t_output = (t_output.split('stdout=b'))[1]
-IP = (terminaloutput.split())[0]
+# print(t_output) #uncomment this if you want to know the index to use (see below) 
+IP = (terminaloutput.split())[0] #maybe you'll need to change the index
 IP = IP[1:]
